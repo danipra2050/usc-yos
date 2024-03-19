a9b2f98f6e7c8df3206c3166a589e73d5eefa02d
Daniela Prado <danipra2050@gmail.com>
2024-03-18 21:03:02 -0500

    Configuration file for APPWRITE

---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+const config = {
+    appUrl: String(import.meta.env.YOS_APPWRITE_URL),
+    appProjectId: String(import.meta.env.YOS_APPWRITE_PROJECT_ID),
+    appDatabaseId: String(import.meta.env.YOS_APPWRITE_DATABASE_ID),
+    appCollectionId: String(import.meta.env.YOS_APPWRITE_COLLECTION_ID),
+    appBucketId: String(import.meta.env.YOS_APPWRITE_BUCKET_ID),
+}
