f106f2446d82027aae0099b7127d4d0de5173d7f
camilosaldarriaga <camilosaldarriaga13@gmail.com>
2024-04-15 20:04:48 -0500

    cambios-15/04/24

--- src/components/loginform.jsx
+++ src/components/loginform.jsx
@@ -1,36 +1,43 @@
 import React from 'react';
-import './loginform.css'; 
-import { TiShoppingCart,FaUser,FaLock } from "react-icons/ti"; 
+import { IonContent, IonInput, IonButton, IonIcon, IonCheckbox, IonLabel, IonItem, IonPage, IonText } from '@ionic/react';
+import { cartOutline, personOutline, lockClosedOutline } from 'ionicons/icons';
+import './loginform.css';
 
-const loginform = () => {
+const LoginForm = () => {
   return (
-    <div className='wrapper'>
-        <form action="">
-        <TiShoppingCart />
+    <IonPage>
+      <IonContent>
+        <div className="wrapper">
+          <form>
+            <IonIcon icon={cartOutline} />
             <h1>YOUR OWN SHOP</h1>
-            <div className="input-box">
-                <input type="text"placeholder='Email'required />
-                <FaUser className='icon'/>
-            </div>
-            <div className="input-box">
-                <input type="password"placeholder='password'required />
-                <FaLock className='icon'/>
-            </div>
+            <IonItem className="input-box">
+              <IonInput type="text" placeholder="Email" required />
+              <IonIcon icon={personOutline} slot="start" />
+            </IonItem>
+            <IonItem className="input-box">
+              <IonInput type="password" placeholder="Password" required />
+              <IonIcon icon={lockClosedOutline} slot="start" />
+            </IonItem>
 
-            <div className="forgot-password">
-                <label ><input type="checkbox" />Recordar contraseña</label>
+            <div className="remember-forgot">
+              <IonCheckbox />
+              <IonLabel>Recordar contraseña</IonLabel>
+              <IonText>
                 <a href="#">olvido la contraseña?</a>
+              </IonText>
             </div>
 
-            <button type="submit">Ingresar</button>
+            <IonButton expand="block" type="submit">Ingresar</IonButton>
 
             <div className="register-link">
-                <p>No tienes Cuenta? <a href="#">Crear Cuenta</a></p>
+              <p>No tienes Cuenta? <a href="#">Crear Cuenta</a></p>
             </div>
-        </form>
-       
-    </div>
+          </form>
+        </div>
+      </IonContent>
+    </IonPage>
   );
 };
 
-export default loginform;
+export default LoginForm;
